02911dc31802fc8882b230e523c80ddbbb8c3f3f
Major <yoyov5123@163.com>
2026-02-14 10:30:28 +0800

    solution

--- src/main/java/codefinity/dao/impl/DepartmentDaoImpl.java
+++ src/main/java/codefinity/dao/impl/DepartmentDaoImpl.java
@@ -2,15 +2,56 @@ package codefinity.dao.impl;
 
 import codefinity.dao.DepartmentDao;
 import codefinity.model.Department;
+import codefinity.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class DepartmentDaoImpl implements DepartmentDao {
+
+
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+
     @Override
     public Department add(Department department) {
-        return null;
+
+        Session currentSession =null ;
+        Transaction transaction =null;
+        try {
+            currentSession = sessionFactory.openSession();
+            transaction = currentSession.beginTransaction();
+
+            currentSession.persist(department);// 自动生成了id,然后自动协会了deparment对象
+            transaction.commit();
+
+        } catch (Exception ex) {
+            if(transaction != null) {
+                transaction.rollback();
+                throw new RuntimeException("Cannot add new department",ex);
+            }
+        } finally {
+            if (currentSession != null) {
+                currentSession.close();
+            }
+        }
+
+        return department;
     }
 
     @Override
     public Department getById(int id) {
-        return null;
+
+
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Department.class, id);
+
+        } catch (Exception ex) {
+            throw new RuntimeException("Cannot get department by id" + id, ex);
+        }
+
+
+
+
     }
 }
--- src/main/java/codefinity/service/impl/DepartmentServiceImpl.java
+++ src/main/java/codefinity/service/impl/DepartmentServiceImpl.java
@@ -1,21 +1,38 @@
 package codefinity.service.impl;
 
+import codefinity.dao.DepartmentDao;
+import codefinity.dao.impl.DepartmentDaoImpl;
 import codefinity.model.Department;
 import codefinity.service.DepartmentService;
 
+
 public class DepartmentServiceImpl implements DepartmentService {
+
+   private  final   DepartmentDao departmentDao = new DepartmentDaoImpl();
+
     @Override
     public Department add(Department department) {
-        return null;
+
+        return departmentDao.add(department);
+
     }
 
     @Override
     public Department getById(int id) {
-        return null;
+        return departmentDao.getById(id);
     }
 
     @Override
     public String getDepartmentNameById(int id) {
-        return null;
+
+        Department department = departmentDao.getById(id);
+        String departmentName= department.getName();
+        if(departmentName!=null){
+            return departmentName;
+        } else {
+            throw  new NullPointerException("The department's name is null, " +
+                    "or there is no name for an department with ID " + id);
+        }
+
     }
 }
